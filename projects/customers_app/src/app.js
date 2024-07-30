4f0431c9f8d74b163d4ecb1b71f46209ded3f9ed
RadosavPanic <radosav.panic01@gmail.com>
2024-07-30 07:11:56 +0200

    updated schema field as required and created post, put and delete methods for customers db

--- projects/customers_app/src/app.js
+++ projects/customers_app/src/app.js
@@ -28,10 +28,48 @@ app.get("/api/customers", async (req, res) => {
   }
 });
 
-app.post("/api/customers", (req, res) => {
+app.get("/api/customers/:id", async (req, res) => {
+  try {
+    const { id: customerId } = req.params;
+    const customer = await Customer.findById(customerId);
+    if (!customer) {
+      res.status(404).json({ error: "User not found" });
+    } else {
+      res.json({ customer });
+    }
+  } catch (err) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.put("/api/customers/:id", async (req, res) => {
+  try {
+    const { id: customerId } = req.params;
+    const result = await Customer.replaceOne({ _id: customerId }, req.body);
+    res.json({ updatedCount: result.modifiedCount });
+  } catch (err) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.delete("/api/customers/:id", async (req, res) => {
+  try {
+    const { id: customerId } = req.params;
+    const result = await Customer.deleteOne({ _id: customerId });
+    res.json({ deletedCount: result.deletedCount });
+  } catch (err) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.post("/api/customers", async (req, res) => {
   const customer = new Customer(req.body);
-  customer.save();
-  res.status(201).json({ customer });
+  try {
+    await customer.save();
+    res.status(201).json({ customer });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 const start = async () => {
